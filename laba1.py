f7ce42e04a76850be4d903ce6e886108483b8017
riplecs <dashamelan2311@gmail.com>
2022-10-19 15:33:58 +0300

    found M given C probabilities

--- laba1.py
+++ laba1.py
@@ -46,3 +46,8 @@ for c in C_values:
                 
 print(pd.DataFrame(CM_probs, index = [f'C_{i}' for i in range(n)],
                    columns = [f'M_{j}' for j in range(n)]))
+
+
+M_given_C_probs = np.divide(CM_probs, [C_probs for i in range(n)])[0]
+print(pd.DataFrame(M_given_C_probs, index = [f'C_{i}' for i in range(n)],
+                   columns = [f'M_{j}' for j in range(n)]))
