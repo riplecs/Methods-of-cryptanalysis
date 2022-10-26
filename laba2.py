d0db4491bf729d8602dbf86164c2d2b3e4f89e21
riplecs <dashamelan2311@gmail.com>
2022-10-26 14:04:59 +0300

    bigram freqs

--- laba2.py
+++ laba2.py
@@ -21,5 +21,12 @@ clean_text = re.sub(f'[^{alphabet}]', '', text.lower())
 
 L = len(clean_text)
 alphabet = list(alphabet)
-letters_freqs = [clean_text.count(i)/L for i in alphabet]
-print(pd.DataFrame(np.matrix(letters_freqs), columns = alphabet, index = ['']))
+
+letter_freqs = [clean_text.count(i)/L for i in alphabet]
+print(pd.DataFrame(np.matrix(letter_freqs), columns = alphabet, index = ['']))
+
+bigrams = [f'{i}{j}' for i in alphabet for j in alphabet]
+bigram_freqs = [clean_text.count(i)/(L - 1) for i in bigrams]
+
+print(pd.DataFrame(np.array(bigram_freqs).reshape((31, 31)), 
+                   columns = alphabet, index = alphabet))
