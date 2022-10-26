ba216366f42c223f3d1c77c0637d35d6f3b0f7b0
riplecs <dashamelan2311@gmail.com>
2022-10-26 13:51:03 +0300

    letter freqs

--- laba2.py
+++ laba2.py
@@ -5,12 +5,21 @@ Created on Wed Oct 26 12:47:22 2022
 @author: Daria
 """
 import re
+import numpy as np
+import pandas as pd
+
 
 file = open('znedoleni.txt', 'r')
+
+alphabet = 'абвгдеєжзиійклмнопрстуфхцчшщьюя'
 
 text = ''
 for line in file:
     text += line
-clean_text = re.sub(r'[^a-щьюяєії]', '', text.lower())
+text = text.replace('ґ', 'г')
+clean_text = re.sub(f'[^{alphabet}]', '', text.lower())
 
-
+L = len(clean_text)
+alphabet = list(alphabet)
+letters_freqs = [clean_text.count(i)/L for i in alphabet]
+print(pd.DataFrame(np.matrix(letters_freqs), columns = alphabet, index = ['']))
