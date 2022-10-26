015806b72e09cbf23b44921bcea049ade08b94ab
riplecs <dashamelan2311@gmail.com>
2022-10-26 14:43:29 +0300

    etropy + compl_index

--- laba2.py
+++ laba2.py
@@ -5,13 +5,14 @@ Created on Wed Oct 26 12:47:22 2022
 @author: Daria
 """
 import re
+import math
 import numpy as np
 import pandas as pd
 
 
 file = open('znedoleni.txt', 'r')
 
-alphabet = 'абвгдеєжзиійклмнопрстуфхцчшщьюя'
+alphabet = 'абвгдеєжзиіїйклмнопрстуфхцчшщьюя'
 
 text = ''
 for line in file:
@@ -28,5 +29,14 @@ print(pd.DataFrame(np.matrix(letter_freqs), columns = alphabet, index = ['']))
 bigrams = [f'{i}{j}' for i in alphabet for j in alphabet]
 bigram_freqs = [clean_text.count(i)/(L - 1) for i in bigrams]
 
-print(pd.DataFrame(np.array(bigram_freqs).reshape((31, 31)), 
+print(pd.DataFrame(np.array(bigram_freqs).reshape((32, 32)), 
                    columns = alphabet, index = alphabet))
+
+entropy_per_letter = sum(-f*math.log2(f) for f in letter_freqs)
+print(entropy_per_letter)
+entropy_per_bigram = sum((-f*math.log2(f) if f > 0 else 0) 
+                         for f in bigram_freqs)/2
+print(entropy_per_bigram)
+compl_index =  sum(clean_text.count(bi)*(clean_text.count(bi) - 1)
+                   for bi in bigrams)/(L*(L - 1))
+print(compl_index)
