b25811f71d6382d853e824b6bb36966150e80b39
riplecs <dashamelan2311@gmail.com>
2022-10-31 14:09:47 +0200

    criterion 2.2

--- laba2.py
+++ laba2.py
@@ -112,7 +112,7 @@ def criterion2_0(L, l, text):
     freqs = letter_freqs if l == 1 else bigram_freqs
     threshold  = np.median(freqs)
     Afrq = [Zm[freqs.index(i)] for i in [j for j in freqs if j > threshold]]
-    text = [text[i:i + l] for i in range(L - 1)]
+    text = [text[i:i + l] for i in range(L - l + 1)]
     return all(elem in np.unique(text) for elem in Afrq)
         
 
@@ -121,14 +121,22 @@ def criterion2_1(L, l, text, threshold2 = 0.8):
     freqs = letter_freqs if l == 1 else bigram_freqs
     threshold  = np.median(freqs)
     Afrq = [Zm[freqs.index(i)] for i in [j for j in freqs if j > threshold]]
-    text = [text[i:i + l] for i in range(L - 1)]
+    text = [text[i:i + l] for i in range(L - l + 1)]
     Aaf = [elem for elem in np.unique(text) if elem in Afrq]
     return len(list(set(Afrq)&set(Aaf))) > len(Afrq)*threshold2
     
+def criterion2_2(L, l, text, threshold2 = 0.001):
+    Zm = alph if l == 1 else bigrams
+    freqs = letter_freqs if l == 1 else bigram_freqs
+    threshold  = np.median(freqs)
+    Afrq = [Zm[freqs.index(i)] for i in [j for j in freqs if j > threshold]]
+    text = [text[i:i + l] for i in range(L - l + 1)]
+    Afrq_freqs = [text.count(i)/(L - l + 1) for i in Afrq]
+    return all([f_x >= threshold2 for f_x in Afrq_freqs])
     
 #for L in (10, 100, 1000, 10000):
 #    texts = gen_texts(clean_text, L, (10000 if L != 10000 else 1000))
-
-print(criterion2_1(10000, 2, clean_text[13:13+10000]))
-print(criterion2_1(10000, 2, uniform_text(2, 10000)))
-print(criterion2_1(10000, 2, fibonacci_text(2, 10000)))
+l = 2
+print(criterion2_2(10000, l, clean_text[:10000]))
+print(criterion2_2(10000, l, uniform_text(l, 10000)))
+print(criterion2_2(10000, l, fibonacci_text(l, 10000)))
