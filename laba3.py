f5d70e4b53d40ae747f690138eaae2d1703cd915
riplecs <dashamelan2311@gmail.com>
2022-11-19 14:52:25 +0200

    MitM attack

--- laba3.py
+++ laba3.py
@@ -8,14 +8,13 @@ import gmpy2
 import numpy as np
 
 file = open('SE_15.txt', 'r')
-data = []
-for line in file:
-    data.append(line[5:])
+data = file.readlines()
 file.close()
 
-C = [int(data[i], 16) for i in range(0, len(data), 2)]
-N = [int(data[i], 16) for i in range(1, len(data), 2)]
-t = len(C)
+t = len(data)//2
+C = [int(data[i][5:], 16) for i in range(0, 2*t, 2)]
+N = [int(data[i][5:], 16) for i in range(1, 2*t, 2)]
+
 e = 5
 
 def Chinese_remainder_theorem(values, modulus):
@@ -33,3 +32,28 @@ def SE_attack():
 
         
 print(SE_attack())
+
+
+
+#file = open('15.txt', 'r')
+file = open('MitM_15.txt', 'r')
+data = file.readlines()
+file.close()
+
+C, N = int(data[0][4:], 16), int(data[1][4:], 16)
+
+e = 65537
+l = 20
+#l = 56
+def MitM_attack():
+    T = range(1, 2**(l//2) + 1)
+    T_pow_e = [gmpy2.powmod(T_i, e, N) for T_i in T]
+    print('-')
+    for T_pow_e_i in T_pow_e:
+        C_s = C*gmpy2.invert(T_pow_e_i, N)%N
+        if C_s in T_pow_e:
+            return hex(T[T_pow_e.index(C_s)]*T[T_pow_e.index(T_pow_e_i)])
+    return 'Відкритий текст не було визначено'
+
+
+print(MitM_attack())
